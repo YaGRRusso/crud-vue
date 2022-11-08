a35c1fc5c7aaabde99cdd70b6e8439a0946eee63
Yago Russo <yagrrusso@gmail.com>
2022-11-07 23:33:13 -0300

    ghpages

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: "/vue-crud/",
-  // publicPath: "",
+  // publicPath: "/vue-crud/",
+  publicPath: "",
 });
